910f87a8e7fa3ed483dd2a396f6323017d137162
Bren <register+git@brenhilt.com>
2023-09-04 11:59:36 +0900

    refactor:

--- src/las2mesh.py
+++ src/las2mesh.py
@@ -1,60 +1,83 @@
-import numpy as np
-import open3d as o3d
-import laspy
-import argparse
-from pygltflib.utils import gltf2glb
+from argparse import ArgumentParser
 import os
 
+import laspy
+from numpy import amax, amin, array, empty, full
+from numpy import vstack
+from numpy import column_stack as cstack
+from open3d.io import write_triangle_mesh
+from open3d.geometry import (
+    KDTreeSearchParamKNN,
+    PointCloud,
+    TriangleMesh
+)
+from open3d.utility import (
+    Vector3dVector,
+    VerbosityContextManager,
+    VerbosityLevel
+)
+from open3d.visualization import draw_geometries
+from pygltflib.utils import gltf2glb
 
-las2mesh_version = "Las2Mesh v0.3b"
+
+VERSION = "Las2Mesh v0.3b"
 
 
 # lasファイルから点群を追加する
 def add_points(filename, points, colors):
     las = laspy.read(filename)
     scales = las.header.scales
-    points = np.vstack([points, np.column_stack([las.points.X * scales[0], las.points.Y * scales[1], las.points.Z * scales[2]])])
+    pe2 = [las.points.X * scales[0],
+           las.points.Y * scales[1],
+           las.points.Z * scales[2]]
+    points = vstack([points, cstack(pe2)])
     if hasattr(las.points, 'red'):
         # カラーあり
-        colors = np.vstack([colors, np.column_stack([las.points.red, las.points.green, las.points.blue])/65535.])
+        ce2 = [las.points.red,
+               las.points.green,
+               las.points.blue]
+        colors = vstack([colors, cstack(ce2) / 65535.])
     else:
         # カラーなし(グレー表示)
-        colors = np.vstack([colors, np.column_stack([np.full((len(las.X), 3), 0.5)])])
+        ge2 = [full((len(las.X), 3), 0.5)]
+        colors = vstack([colors, cstack(ge2)])
     return points, colors
 
 
 # 複数の .lasファイルを読み込み1つの点群にする
 def load_files(files):
-    vec = np.empty((0, 3))
-    col = np.empty((0, 3))
+    vec = empty((0, 3))
+    col = empty((0, 3))
 
     for filename in files:
         vec, col = add_points(filename, vec, col)
-        print(filename + " => " + str(len(vec)) + " points(total)")
+        print(f"{filename} => {len(vec)} points(total)")
 
     # 端が原点となるように移動
-    min = np.amin(vec, axis=0)
-    max = np.amax(vec, axis=0)
+    min = amin(vec, axis=0)
+    max = amax(vec, axis=0)
     vec = vec - min
-    bbox = max-min
+    bbox = max - min
     print(f"size: {bbox[0]:.1f} x {bbox[1]:.1f} x {bbox[2]:.1f} (m)")
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(vec)
-    pcd.colors = o3d.utility.Vector3dVector(col)
+    pcd = PointCloud()
+    pcd.points = Vector3dVector(vec)
+    pcd.colors = Vector3dVector(col)
     return pcd
 
 
 # 点群からメッシュを生成
 def create_mesh(point_cloud, mesh_depth):
     # 法線の推定
-    point_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn=20))
+    point_cloud.estimate_normals(search_param=KDTreeSearchParamKNN(knn=20))
 
     # Upの軸を指定する
-    point_cloud.orient_normals_to_align_with_direction(orientation_reference=np.array([0., 0., 1.]))
+    point_cloud.orient_normals_to_align_with_direction(
+        orientation_reference=array([0., 0., 1.])
+    )
 
     # メッシュ化
-    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug):
-        poisson_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+    with VerbosityContextManager(VerbosityLevel.Debug):
+        poisson_mesh, densities = TriangleMesh.create_from_point_cloud_poisson(
             point_cloud, depth=mesh_depth)
     print(poisson_mesh)
 
@@ -69,26 +92,35 @@ def create_mesh(point_cloud, mesh_depth):
 
 
 def write_mesh(filename, mesh):
+    opt = dict(write_ascii=False, write_vertex_normals=True)
     body, ext = os.path.splitext(filename)
     if ext == ".glb":
         gltf_file = body + ".gltf"
-        o3d.io.write_triangle_mesh(gltf_file, mesh, write_ascii=False, write_vertex_normals=True)
+        write_triangle_mesh(gltf_file, mesh, **opt)
         gltf2glb(gltf_file, override=True)
         os.remove(gltf_file)
     else:
-        o3d.io.write_triangle_mesh(filename, mesh, write_ascii=False, write_vertex_normals=True)
+        write_triangle_mesh(filename, mesh, **opt)
 
 
 def main():
-    print(las2mesh_version)
-    parser = argparse.ArgumentParser(description='.lasファイルからメッシュを生成します')
-    parser.add_argument('files', help='対象の .lasファイル。複数指定できます。', nargs='*')
-    parser.add_argument('-d', '--depth', default=10, type=int, help='メッシュの細かさを整数で指定します。デフォルト値は 10 です。')
-    parser.add_argument('-o', '--output', default='output.ply', help='出力ファイル名を指定します。デフォルト値は output.ply です。出力形式は、.ply, .stl, .obj, .off, .gltf に対応しています。')
-    parser.add_argument('-n', '--nopreview', action='store_true', help='3Dプレビュー表示を無効にします')
+    print(VERSION)
+    parser = ArgumentParser(description=".lasファイルからメッシュを生成します")
+    parser.add_argument('files', nargs='*',
+                        help="対象の .lasファイル。複数指定できます。")
+    parser.add_argument('-d', '--depth', default=10, type=int,
+                        help=("メッシュの細かさを整数で指定します。"
+                              "デフォルト値は 10 です。"))
+    parser.add_argument('-o', '--output', default='output.ply',
+                        help=("出力ファイル名を指定します。"
+                              "デフォルト値は output.ply です。出力形式は、"
+                              ".ply, .stl, .obj, .off, .gltf"
+                              " に対応しています。"))
+    parser.add_argument('-n', '--nopreview', action='store_true',
+                        help="3Dプレビュー表示を無効にします")
     args = parser.parse_args()
 
-    if len(args.files)==0:
+    if len(args.files) == 0:
         parser.print_help()
         return
 
@@ -100,10 +132,13 @@ def main():
     write_mesh(output_path, mesh)
 
     if not args.nopreview:
-        # o3d.visualization.draw(mesh) # 詳細設定用
+        # from open3d.visualization import draw,
+        # draw(mesh) # 詳細設定用
         # 画面表示
-        o3d.visualization.draw_geometries([mesh], window_name=las2mesh_version + " - " + output_path + " (preview)")
-        # 操作方法 http://www.open3d.org/docs/latest/tutorial/Basic/visualization.html
+        wname = f"{VERSION} - {output_path} (preview)"
+        draw_geometries([mesh], window_name=wname)
+        # 操作方法:
+        # http://www.open3d.org/docs/latest/tutorial/Basic/visualization.html
 
 
 if __name__ == '__main__':
